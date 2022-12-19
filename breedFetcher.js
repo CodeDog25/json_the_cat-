7e4431b629389ff71f7190b2898b6f7905daff51
Sicily Ding <xishading@gmail.com>
2022-12-18 23:01:27 -0700

    Refactored breedFetcher.js

---
+++ breedFetcher.js
@@ -0,0 +1,19 @@
+const request = require("request");
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback(`The breed ${breedName} is not found`);
+      return;
+    } else {
+      callback(null, data[0].description);
+      return;
+    }
+  });
+};
+  
+module.exports = { fetchBreedDescription };
